73ad5245b27c3a9db4def324fdec0d30b90ecc7d
Jonathan Chhabra <fresh@freshemail.fresh>
2017-04-16 20:27:09 -0400

    Configure Auth0 to use local environment variables

--- server/server.js
+++ server/server.js
@@ -6,13 +6,15 @@ const jwt = require('express-jwt');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+require('dotenv').config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+
 const authCheck = jwt({
-  secret: 'AUTH0_CLIENT_SECRET',
-  audience: 'AUTH0_CLIENT_ID'
+  secret: process.env.AUTH0_CLIENT_SECRET,
+  audience: process.env.AUTH0_CLIENT_ID
 });
 
 app.get('/api/data', (req, res) => {
